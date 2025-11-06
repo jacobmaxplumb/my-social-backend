9b628d9ceec6f3562a6253c76ebbf7228121e014
jacob Plumb <jacobplumb@jacobs-MacBook-Pro.local>
2025-11-06 17:26:50 -0500

    test

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { requireAuth } = require('../middleware/auth');
 const { getUserData, enrichPosts, createPost, addComment, getRelativeTimestamp, togglePostLike, toggleCommentLike } = require('../mockData');
-
+// coment
 /**
  * @swagger
  * /posts:
